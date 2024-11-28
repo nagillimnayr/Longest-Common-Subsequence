a4c6d3e56bca59137bacff940313ebd44c5e9b87
Ryan Milligan <rcm9@sfu.ca>
2024-11-28 14:21:15 -0800

    move files into project/

--- generate_sequences.py
+++ generate_sequences.py
@@ -19,7 +19,7 @@ def generate_data_set(length):
     'sequence_a': [generate_sequence(length) for _ in range(n_sequences)],
     'sequence_b': [generate_sequence(length) for _ in range(n_sequences)]
   })
-  df.to_csv(f'data/random/sequences_{length}.csv', header=False, index=False)
+  df.to_csv(f'project/data/random/sequences_{length}.csv', header=False, index=False)
 
 def main():
   
